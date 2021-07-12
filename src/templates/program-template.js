4908282f9a216721569c655c37cbb9f2786706df
Bobby Smith <reversetype@gmail.com>
2021-07-12 13:58:33 -0400

    group by colleges

--- src/templates/program-template.js
+++ src/templates/program-template.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
-import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel'
 import '@brainhubeu/react-carousel/lib/style.css'
 
 import Layout from '../components/Layout'
